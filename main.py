5f3023343f85b3fefa479efb3cb91d7112c0b077
Sebastian Specht <sebi@sieglithof.net>
2023-10-23 18:51:02 +0200

    basic structure

---
+++ main.py
@@ -0,0 +1,8 @@
+from kauderwelsch import *
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: main.py <file>")
+    file = sys.argv[1]
+    # TODO: Compile and run file
